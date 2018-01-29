7de76a93f67c4c7790e08dd6d6ee3e2be69d163a
gossion <guwe@microsoft.com>
2018-01-29 01:35:40 +0000

    draft2

--- src/bosh_azure_cpi/lib/cloud/azure/telemetry.rb
+++ src/bosh_azure_cpi/lib/cloud/azure/telemetry.rb
@@ -1,8 +1,23 @@
 module Bosh::AzureCloud
   class Telemetry
-    def initialize(operation)
+    def initialize(operation)#, subscription_id: nil, vm_size: nil, disk_size: nil)
       @logger = Bosh::Clouds::Config.logger
       @operation = operation
+      #@subscription_id = subscription_id #is this a sensitive info?
+      #@vm_size = vm_size
+      #@disk_size = disk_size
+
+      # parameters:
+      #   "Name": "BOSH-CPI"
+      #   "Version": ""
+      #   "Operation": CPI callback, e.g. create_vm, create_disk, attach_disk, etc
+      #   "OperationSuccess": true / false
+      #   "Duration": time
+      #   "Message": A JSON string contains info of
+      #      "msg": "Successed" or error message
+      #      "vm_size": vm size or nil
+      #      "disk_size": disk size or nil
+      #      "subscription_id": subscription id or nil
       @event = {
         "eventId" => 1,
         "providerId" => "69B669B9-4AF8-4C50-BDC4-6006FA76E975",
@@ -14,31 +29,25 @@ module Bosh::AzureCloud
           {
             "name" => "Version",
             "value" => ""
-          },
-          {
-            "name" => "IsInternal",
-            "value" => false
-          },
-          {
-            "name" => "Operation",
-            "value" => @operation
-          },
-          {
-            "name" => "ExtensionType",
-            "value" => ""
           }
         ]
       }
-      # location
-      # vm size
-      # disk size
-      # environment
     end
 
-    def with_telemetry
+    # extras:
+    #   {"vm_size" => "Standard_D1"}
+    #   {"disk_size" => 1024}
+    def monitor(extras = {})
+      event_param_operation = {"name" => "Operation", "value" => @operation}
       event_param_operation_success = {"name" => "OperationSuccess", "value" => true}
-      event_param_message = {"name" => "Message", "value" => "Successed"}
-      event_param_duration = {"name" => "Duration", "value" => 0}
+      event_param_duration = {"name" => "Duration"}
+      event_param_message = {"name" => "Message"}
+
+      message_value = {"msg" => "Successed"}
+      message_value.merge!(extras)
+      #message_value["subscription_id"] = @subscription_id unless @subscription_id.nil?
+      #message_value["vm_size"] = @vm_size unless @vm_size.nil?
+      #message_value["disk_size"] = @disk_size unless @disk_size.nil?
 
       start_at = Time.now
       begin
@@ -47,11 +56,13 @@ module Bosh::AzureCloud
         event_param_operation_success["value"] = false
         message = "#{e.inspect}\n#{e.backtrace.join("\n")}"
         message = message[0...3990] + '...' if message.length > 3993 #limit the message to less than 3.9 kB
-        event_param_message["value"] = message
+        message_value["msg"] = message
         raise e
       ensure
         end_at = Time.now
         event_param_duration["value"] = (end_at - start_at) * 1000.0
+        event_param_message["value"] = message_value.to_json
+        @event["parameters"] << event_param_operation
         @event["parameters"] << event_param_operation_success
         @event["parameters"] << event_param_message
         @event["parameters"] << event_param_duration
