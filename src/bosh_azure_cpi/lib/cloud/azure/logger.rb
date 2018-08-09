47c1d00be6b8e0e94252b6509ebce0f91d07a819
gossion <guwe@microsoft.com>
2018-08-09 07:02:00 +0000

    logger file

--- src/bosh_azure_cpi/lib/cloud/azure/logger.rb
+++ src/bosh_azure_cpi/lib/cloud/azure/logger.rb
@@ -1,15 +1,9 @@
 module Bosh::AzureCloud
   class CPILogger
     def self.get_logger(device)
-      Logging.appenders.stdout(
-        :layout => Logging.layouts.pattern(
-        :pattern =>  "%.1l, [%d #%p #%t] %l --%X{flag}: %m\n", # flag is a placeholder for request id
-        :date_pattern  => '%Y-%m-%dT%H:%M:%S.%6N'
-        )
-      )
-
-      logger = Logging.logger['CPI']
-      logger.add_appenders 'stdout'
+      logger = Logging.logger(device,
+                              pattern: '%.1l, [%d #%p #%t] %l --%X{flag}: %m\n', # flag is a placeholder for request id
+                              date_pattern: '%Y-%m-%dT%H:%M:%S.%6N')
       logger.level = :debug
       logger
     end
