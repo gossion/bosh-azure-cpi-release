ffeb069d103184978f263ec8307a9da5d048ecbd
gossion <guwe@microsoft.com>
2018-09-14 07:10:21 +0000

    Support cloud-init

--- src/bosh_azure_cpi/lib/cloud/azure/utils/bosh_agent_util.rb
+++ src/bosh_azure_cpi/lib/cloud/azure/utils/bosh_agent_util.rb
@@ -29,8 +29,8 @@ module Bosh::AzureCloud
     #   }
     # }
     def self.get_encoded_user_data(registry_endpoint, instance_id, dns, computer_name = nil)
-      user_data = get_user_data_obj(registry_endpoint, instance_id, dns, computer_name)
-      Base64.strict_encode64(JSON.dump(user_data))
+      cloud_init_config = get_user_data_obj(registry_endpoint, instance_id, dns, computer_name)
+      Base64.strict_encode64(cloud_init_config)
     end
 
     def self.get_user_data_obj(registry_endpoint, instance_id, dns, computer_name = nil)
@@ -42,7 +42,25 @@ module Bosh::AzureCloud
         user_data[:server] = { name: instance_id }
       end
       user_data[:dns] = { nameserver: dns } if dns
-      user_data
+
+      cloud_init_config = "#cloud-config
+# vim: syntax=yaml
+#
+# This is the configuration syntax that the write_files module
+# will know how to understand. encoding can be given b64 or gzip or (gz+b64).
+# The content will be decoded accordingly and then written to the path that is
+# provided.
+#
+# Note: Content strings here are truncated for example purposes.
+write_files:
+-   content: |
+      #{JSON.dump(user_data)}
+    owner: root:root
+    path: /var/lib/cloudinit/CustomData
+    permissions: '0644'
+"
+
+      cloud_init_config
     end
 
     def self.get_meta_data_obj(instance_id, ssh_public_key)
